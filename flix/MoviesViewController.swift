c9aa54f4f8cbdab62553c6e106adf47022bdc873
yuichu <23323904+yuichu@users.noreply.github.com>
2022-02-23 23:11:03 -0800

    network request using themoviedb api

--- flix/MoviesViewController.swift
+++ flix/MoviesViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MoviesViewController.swift
 //  flix
 //
 //  Created by Yujin Chung on 2/23/22.
@@ -7,11 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MoviesViewController: UIViewController {
+    
+    // properties
+    var movies = [[String:Any]]()   // creation of array of dictionaries
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("hello!")
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+             // This will run when the network request returns
+             if let error = error {
+                    print(error.localizedDescription)
+             } else if let data = data {
+                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                    
+                    self.movies = dataDictionary["results"] as! [[String:Any]] // access key results
+
+                    // TODO: Get the array of movies
+                    // TODO: Store the movies in a property to use elsewhere
+                    // TODO: Reload your table view data
+
+             }
+        }
+        task.resume()
+
+
+
+
     }
 
 
