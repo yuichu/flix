a3f011649ee6480ddbb1733cbda58165394d7d06
yuichu <23323904+yuichu@users.noreply.github.com>
2022-02-24 17:23:04 -0800

    updated table view stubs to display current movies. Need to implement update count

--- flix/MoviesViewController.swift
+++ flix/MoviesViewController.swift
@@ -12,16 +12,13 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     @IBOutlet weak var tableView: UITableView!
     
     // properties
-    var movies = [[String:Any]]()   // creation of array of dictionaries
+    var movies = [[String:Any]]()   // array of dictionaries
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
         tableView.dataSource = self
         tableView.delegate = self
-        
-        print("hello!")
         
         // network request snippet to get new movies
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
@@ -34,13 +31,8 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
              } else if let data = data {
                     let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                     
-                    // access key results
+                    // access key results from API and store to movies property
                     self.movies = dataDictionary["results"] as! [[String:Any]]
-
-                    // TODO: Get the array of movies
-                    // TODO: Store the movies in a property to use elsewhere
-                    // TODO: Reload your table view data
-
              }
         }
         task.resume()
@@ -49,15 +41,20 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     // protocol stub - number of rows
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50
+        return movies.count
     }
     // protocol stub - display the rows
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // get cell for particular row
+        // create cell for particular row
         let cell = UITableViewCell()
         
-        // print indexPath. Note syntax "\(variable)"
-        cell.textLabel!.text = "row: \(indexPath.row)"
+        // get current movie per API results index
+        let movie = movies[indexPath.row]
+        // get title of movie (cast to string)
+        let title = movie["title"] as! String
+        
+        // display title in cell
+        cell.textLabel!.text = title
         
         return cell
     }
