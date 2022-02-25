dc9ed33857e731c3abd2e8bc36533ee064c5feb0
yuichu <23323904+yuichu@users.noreply.github.com>
2022-02-24 18:48:02 -0800

    created MovieCell class for outlets within table view

---
+++ flix/MovieCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieCell.swift
+//  flix
+//
+//  Created by Yujin Chung on 2/24/22.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
