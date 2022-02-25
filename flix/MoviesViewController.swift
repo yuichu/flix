958b669dacefe8fc9f4f7b950455ee63a3b534ac
yuichu <23323904+yuichu@users.noreply.github.com>
2022-02-24 16:25:33 -0800

    initialized movies table view

--- flix/MoviesViewController.swift
+++ flix/MoviesViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class MoviesViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    
     // properties
     var movies = [[String:Any]]()   // creation of array of dictionaries
 
@@ -18,6 +20,7 @@ class MoviesViewController: UIViewController {
         
         print("hello!")
         
+        // network request snippet to get new movies
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -28,7 +31,8 @@ class MoviesViewController: UIViewController {
              } else if let data = data {
                     let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                     
-                    self.movies = dataDictionary["results"] as! [[String:Any]] // access key results
+                    // access key results
+                    self.movies = dataDictionary["results"] as! [[String:Any]]
 
                     // TODO: Get the array of movies
                     // TODO: Store the movies in a property to use elsewhere
@@ -37,7 +41,7 @@ class MoviesViewController: UIViewController {
              }
         }
         task.resume()
-
+        // end network request snippet
 
 
 
